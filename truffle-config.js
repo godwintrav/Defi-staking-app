66897bfadcad25ad4ec99389a2fa127e68591107
Odenigbo Godwin <godwintrav@gmail.com>
2022-07-29 08:37:32 +0000

    final changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,22 @@
 require('babel-register');
 require('babel-polyfill');
+const provider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const secrets = JSON.parse(
+  fs.readFileSync('.secrets.json').toString().trim()
+);
 
 module.exports = {
     networks: {
+        goerli: {
+            provider: () => new provider(
+             secrets.privateKeys,
+              'https://goerli.infura.io/v3/d7509ac78da84338b5430611d7223c93',
+              0,
+              3
+            ),
+            network_id: 5
+          },
         development: {
             host:'127.0.0.1',
             port: 7545,
